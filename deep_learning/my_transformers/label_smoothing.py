f80ee3970c309d669fc122f66f31c477b26b33c5
gpurci <gepurice@gmail.com>
2025-12-25 17:50:36 +0200

    update: corecteaza valoarea maxima

--- deep_learning/my_transformers/label_smoothing.py
+++ deep_learning/my_transformers/label_smoothing.py
@@ -10,6 +10,7 @@ class LabelSmoothing(v2.Transform):
         assert (num_classes > 0), "Error: num_classes '{}' should be greater than '0'".format(num_classes)
         self.num_classes = num_classes
         self.smooth_size = smooth_size
+        self.on_value = 1 - smooth_size
         self.dtype = dtype
         
     def __call__(self, x, y):
@@ -18,5 +19,5 @@ class LabelSmoothing(v2.Transform):
         label_smoothing = torch.empty((y.shape[0], self.num_classes), dtype=self.dtype, requires_grad=False).to(y.device)
         label_smoothing.uniform_(0, self.smooth_size)
         # 
-        y = F.one_hot(y, num_classes=self.num_classes).type(self.dtype) * 0.8
+        y = F.one_hot(y, num_classes=self.num_classes).type(self.dtype) * self.on_value
         return x, label_smoothing+y
