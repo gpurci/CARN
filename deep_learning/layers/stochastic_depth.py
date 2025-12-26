e9f686ec5067df504feeccb2162a43a951c575f1
gpurci <gepurice@gmail.com>
2025-12-26 17:22:04 +0200

    add: stochastic_depth

---
+++ deep_learning/layers/stochastic_depth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import torch
+from torch import nn
+
+class StochasticDepth(nn.Module):
+   def __init__(self, p=0.5, shape=()):
+      super().__init__()
+      assert (p >= 0), "Error: percent '{}' should be greater equal to '0'".format(p)
+      assert (p <= 1), "Error: percent '{}' should be less equal to '1'".format(p)
+      self.surv_prob = p
+      self.shape = shape
+
+   def reset_parameters(self):
+      pass
+
+   def forward(self, x):
+      if (self.training):
+         dropout_tensor = torch.rand((x.shape[0], *self.shape), 
+               device=x.device, requires_grad=False, dtype=torch.float32) < self.surv_prob
+         dropout_tensor = torch.div(dropout_tensor, self.surv_prob)
+         x *= dropout_tensor
+      return x
---
+++ deep_learning/layers/stochastic_depth_add.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import torch
+from torch import nn
+
+class StochasticDepthAdd(nn.Module):
+   def __init__(self, p=0.5, shape=()):
+      super().__init__()
+      assert (p >= 0), "Error: percent '{}' should be greater equal to '0'".format(p)
+      assert (p <= 1), "Error: percent '{}' should be less equal to '1'".format(p)
+      self.surv_prob = p
+      self.shape = shape
+
+   def reset_parameters(self):
+      pass
+
+   def forward(self, x1, x0):
+      if (self.training):
+         dropout_tensor = torch.rand((x0.shape[0], *self.shape), 
+               device=x0.device, requires_grad=False, dtype=torch.float32) < self.surv_prob
+         x0 *= dropout_tensor
+         x1 *= torch.inverse(dropout_tensor)
+         x0 = torch.div(x0, self.surv_prob)
+         x1 = torch.div(x1, self.surv_prob)
+
+      return x0+x1
