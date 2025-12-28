5400c732249969f27bfedfb68fe3ec6af2921a99
gpurci <gepurice@gmail.com>
2025-12-28 20:49:45 +0200

    add: adauga callbacks si metrics la training

--- deep_learning/trainer/supervised_callback_metrics_trainer.py
+++ deep_learning/trainer/supervised_callback_metrics_trainer.py
@@ -4,12 +4,21 @@ import torch
 from tqdm import tqdm
 from torch import nn
 
+from sys_function import * # este in root
+sys_remove_modules("callback.callback_list")
+sys_remove_modules("metrics.metrics_list")
+
+from callback.callback_list import *
+from metrics.metrics_list import *
+
 class SupervisedCallbackMetricsTrainer():
    def __init__(self,
          model: nn.Module,
          optimizer: torch.optim.Optimizer,
          criterion: nn.Module,
-         device="cuda",
+         device:"cuda",
+         callbacks:list = [],
+         metrics:dict = {},
          type_compile="normal",
          disable_tqdm: bool = False, 
          transforms=None, 
@@ -47,6 +56,9 @@ class SupervisedCallbackMetricsTrainer():
       self.best_va_acc  = 0.0
       # 
       self.lr_scheduler = lr_scheduler
+      #
+      self.callbacks = CallbacksList(callbacks)
+      self.metrics   = MetricsList(metrics)
 
    def setOptimizer(self, optimizer, lr_scheduler=None):
       self.optimizer    = optimizer
@@ -63,13 +75,8 @@ class SupervisedCallbackMetricsTrainer():
    def train(self, train_ds):
       self.model.train()
 
-      total = 0
-      correct = 0
-      total_loss = 0
-
-      start_lr = self.optimizer.lr
-      mean_lr  = start_lr
-      for data, target in tqdm(train_ds, desc="Training", leave=False, disable=self.disable_tqdm):  # Disable on notebook
+      for batch, (data, target) in enumerate(train_ds, 0):
+         self.callbacks.on_train_batch_begin(batch, None)
          # We must move the data to the same device as the model
          # We can also use non_blocking=True to speed up the transfer for large tensors
          # Works when using pin_memory=True. For more details, check the references for pinning memory.
@@ -83,7 +90,7 @@ class SupervisedCallbackMetricsTrainer():
          if (self.all_transforms is not None):
             data, target = self.all_transforms(data, target)
          predicted, loss = self.step(data, target)
-         mean_lr += self.optimizer.lr
+
          if (self.lr_scheduler is not None):
             self.lr_scheduler.step()
          # This metric is actually an approximation of an accuracy, we are checking whether the dominant class
@@ -93,11 +100,9 @@ class SupervisedCallbackMetricsTrainer():
          # We use detach because we tell the autograd engine to not track the gradients for predicted anymore
          predicted = predicted.detach().cpu().argmax(dim=1)
          target    = target.detach().cpu().argmax(dim=1)
-         correct += predicted.eq(target).sum().item()
-         total   += data.size(0)
-         total_loss += float(loss.item())
-
-      return total_loss / total, correct / total, start_lr, mean_lr / total
+         loss = float(loss.item())
+         logs = self.metrics(target, predicted, loss=loss)
+         self.callbacks.on_train_batch_end(batch, logs)
 
    # Here we use the inference_mode. We are telling pytorch we are doing just inference, we don't need to track
    # tensor operations with the Autograd engine for automatic differentiation. This is also what torch.no_grad() does.
@@ -114,11 +119,8 @@ class SupervisedCallbackMetricsTrainer():
    def val(self, val_ds):
       self.model.eval()
 
-      total = 0
-      correct = 0
-      total_loss = 0
-
-      for data, target in tqdm(val_ds, desc="Validation", leave=False, disable=self.disable_tqdm):  # Disable on notebook
+      for batch, (data, target) in enumerate(val_ds, 0):
+         self.callbacks.on_test_batch_begin(batch, None)
          # go to device
          data   = data.to(self.device, non_blocking=True)
          target = target.to(self.device, non_blocking=True)
@@ -130,31 +132,20 @@ class SupervisedCallbackMetricsTrainer():
          # We don't need to detach, because we are already in inference_mode
          predicted = predicted.detach().cpu().argmax(dim=1)
          target    = target.detach().cpu()
-         correct += predicted.eq(target).sum().item()
-         total   += data.size(0)
-         total_loss += loss
-
-      return total_loss / total, correct / total
+         logs = self.metrics(target, predicted, "val_", loss=loss)
+         self.callbacks.on_test_batch_end(batch, logs)
 
    def run(self, train_dl, val_dl, epochs: int, save_path:str):
       print(f"Running {epochs} epochs")
-      with tqdm(range(epochs), desc="Training") as pbar:
-         for _ in pbar:
-            tr_loss, tr_acc = self.train(train_dl)
-            va_loss, va_acc = self.val(val_dl)
-            print({"tr_loss":tr_loss, "tr_acc":tr_acc, "va_loss":va_loss, "va_acc":va_acc})
-            if va_acc > self.best_va_acc:
-               self.best_va_acc = va_acc
-               torch.save({
-                  'model_state_dict': self.model.state_dict(),
-                  'model_name': self.model.name,
-               }, save_path)
-            pbar.set_postfix(
-                           train_loss=float(tr_loss),
-                           train_acc=float(tr_acc),
-                           val_loss=float(va_loss),
-                           val_acc=float(va_acc),)
-      return {"tr_loss":tr_loss, "tr_acc":tr_acc, "va_loss":va_loss, "va_acc":va_acc}
+      for epoch in range(epochs):
+         self.train(train_dl)
+         train_logs = self.metrics.logs()
+         self.callbacks.on_train_end(train_logs)
+         self.val(val_dl)
+         val_logs = self.metrics.logs()
+         self.callbacks.on_test_end(val_logs)
+         train_logs.update(val_logs)
+         self.callbacks.on_epoch_end(epoch, train_logs)
       # We use tqdm to have a progress bar for the epochs. We disable inner progress bars on jupyter notebooks,
       # because either they produce a lot of output, or disable loading the notebook on GitHub.
       # If you run this script on a terminal, you can enable the inner progress bars.
