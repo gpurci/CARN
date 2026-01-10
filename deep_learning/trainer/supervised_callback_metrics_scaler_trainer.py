2be9e683dcb8fbab740895dd3adcf77cbb8919e6
gpurci <gepurice@gmail.com>
2026-01-10 05:38:00 +0200

    update: adauga scalare la training

--- deep_learning/trainer/supervised_callback_metrics_scaler_trainer.py
+++ deep_learning/trainer/supervised_callback_metrics_scaler_trainer.py
@@ -2,7 +2,6 @@
 
 import os
 import torch
-from tqdm import tqdm
 from torch import nn
 import warnings
 
@@ -25,7 +24,6 @@ class SCMTrainer():
          transforms=None, 
          all_transforms=None, 
          lr_scheduler=None,
-         in_shape=None
    ):
       self.device = device
       print(f"Using device: {self.device}")
@@ -63,62 +61,54 @@ class SCMTrainer():
             warnings.warn("\n\ntorch.compile is not supported on Windows. Try Linux or WSL instead.\n\n")
          else: # TO DO: check if is linux
             # This compiles the model. See https://docs.pytorch.org/tutorials/intermediate/torch_compile_tutorial.html
-            self.model = torch.compile(self.model)
-      elif (model_type == "frozen"):
-         self.model = torch.jit.freeze(torch.jit.script(self.model))
-      elif (model_type == "optimize_for_inference"):
-         self.model = torch.jit.optimize_for_inference(torch.jit.script(self.model))
-      elif (model_type == "traced"):
-         if (in_shape is not None):
-            data = torch.rand(in_shape, device=self.device)
-            self.model = torch.jit.trace(self.model, data)
-         else:
-            raise RuntimeError("The 'in_shape' is None '{}'".format(in_shape))
+            self.model.compile()
+            # This compiles the step function
+            self.step = torch.compile(self.step)
       else:
          raise RuntimeError("std::unreachable")
-
-
-
+ 
    def setOptimizer(self, optimizer, lr_scheduler=None):
       self.optimizer    = optimizer
       self.lr_scheduler = lr_scheduler
 
-   def step(self, data: torch.Tensor, target: torch.Tensor):
-      predicted = self.model(data)
-      loss = self.criterion(predicted, target)
-      loss.backward()
-      self.optimizer.step()
+   def step(self, inputs: torch.Tensor, targets: torch.Tensor):
+      with torch.autocast(self.device.type, enabled=self.enable_half):
+         predicted = self.model(inputs)
+         loss      = self.criterion(predicted, targets)
+      self.scaler.scale(loss).backward()
+      self.scaler.step(self.optimizer)
+      self.scaler.update()
       self.optimizer.zero_grad()
       return predicted, loss
 
    def train(self, train_ds):
       self.model.train()
 
-      for batch, (data, target) in enumerate(train_ds, 0):
+      for batch, (inputs, targets) in enumerate(train_ds, 0):
          #self.callbacks.on_train_batch_begin(batch, None)
-         # We must move the data to the same device as the model
+         # We must move the dataset to the same device as the model
          # We can also use non_blocking=True to speed up the transfer for large tensors
          # Works when using pin_memory=True. For more details, check the references for pinning memory.
          # but this is useful only for pinned memory transfers (CPU-to-GPU)
          # In most cases, the improvement is negligible
-         data   = data.to(self.device, non_blocking=True)
-         target = target.to(self.device, non_blocking=True)
+         inputs  = inputs.to(self.device,  non_blocking=True)
+         targets = targets.to(self.device, non_blocking=True)
 
          if (self.transforms is not None):
-            data = self.transforms(data)
+            inputs = self.transforms(inputs)
          if (self.all_transforms is not None):
-            data, target = self.all_transforms(data, target)
-         predicted, loss = self.step(data, target)
+            inputs, targets = self.all_transforms(inputs, targets)
+         predicted, loss = self.step(inputs, targets)
 
          # This metric is actually an approximation of an accuracy, we are checking whether the dominant class
          # predicted by the model is also equal to the dominant soft label
-         # The reason we are moving the data from device back to CPU is because these calculations are usually
+         # The reason we are moving the dataset from device back to CPU is because these calculations are usually
          # faster on CPU for small batch sizes
          # We use detach because we tell the autograd engine to not track the gradients for predicted anymore
          predicted = predicted.detach().cpu().argmax(dim=1)
-         target    = target.detach().cpu().argmax(dim=1)
+         targets   = targets.detach().cpu().argmax(dim=1)
          loss = float(loss.item())
-         logs = self.metrics(target, predicted, loss=loss)
+         logs = self.metrics(targets, predicted, loss=loss)
          #self.callbacks.on_train_batch_end(batch, logs)
 
    # Here we use the inference_mode. We are telling pytorch we are doing just inference, we don't need to track
@@ -136,20 +126,20 @@ class SCMTrainer():
    def val(self, val_ds):
       self.model.eval()
 
-      for batch, (data, target) in enumerate(val_ds, 0):
+      for batch, (inputs, targets) in enumerate(val_ds, 0):
          #self.callbacks.on_test_batch_begin(batch, None)
          # go to device
-         data   = data.to(self.device, non_blocking=True)
-         target = target.to(self.device, non_blocking=True)
+         inputs  = inputs.to(self.device,  non_blocking=True)
+         targets = targets.to(self.device, non_blocking=True)
 
-         predicted = self.model(data)
-         loss = self.criterion(predicted, target).item()
+         predicted = self.model(inputs)
+         loss = self.criterion(predicted, targets).item()
 
-         # Here we don't need to argmax the target, because we have hard labels. We don't use DA during validation.
+         # Here we don't need to argmax the targets, because we have hard labels. We don't use DA during validation.
          # We don't need to detach, because we are already in inference_mode
          predicted = predicted.detach().cpu().argmax(dim=1)
-         target    = target.detach().cpu()
-         logs = self.metrics(target, predicted, "val_", loss=loss)
+         targets   = targets.detach().cpu()
+         logs = self.metrics(targets, predicted, "val_", loss=loss)
          #self.callbacks.on_test_batch_end(batch, logs)
 
    def run(self, train_dl, val_dl, epochs: int):
@@ -170,7 +160,7 @@ class SCMTrainer():
       # because either they produce a lot of output, or disable loading the notebook on GitHub.
       # If you run this script on a terminal, you can enable the inner progress bars.
       # Some more details about efficiency:
-      #  * Using pin_memory=True in the DataLoader usually increases the data transfer speed from
+      #  * Using pin_memory=True in the DataLoader usually increases the dataset transfer speed from
       #    CPU RAM to GPU RAM, using pinned memory. More details in the official documentation.
       #    The downside is that pinned memory is a limited resource, and allocating too much of it can lead to
       #    system instability. Therefore, monitor your system when using pin_memory=True.
@@ -194,20 +184,20 @@ class SCMTrainer():
       correct = 0
       total_loss = 0
 
-      for data, target in tqdm(eval_dl, desc="Validation", leave=False):  # Disable on notebook
+      for inputs, targets in tqdm(eval_dl, desc="Validation", leave=False):  # Disable on notebook
          # go to device
-         data   = data.to(self.device, non_blocking=True)
-         target = target.to(self.device, non_blocking=True)
+         inputs  = inputs.to(self.device,  non_blocking=True)
+         targets = targets.to(self.device, non_blocking=True)
 
-         predicted = self.model(data)
-         loss = self.criterion(predicted, target).item()
+         predicted = self.model(inputs)
+         loss = self.criterion(predicted, targets).item()
 
-         # Here we don't need to argmax the target, because we have hard labels. We don't use DA during validation.
+         # Here we don't need to argmax the targets, because we have hard labels. We don't use DA during validation.
          # We don't need to detach, because we are already in inference_mode
          predicted = predicted.detach().cpu().argmax(dim=1)
-         target    = target.detach().cpu()
-         correct += predicted.eq(target).sum().item()
-         total   += data.size(0)
+         targets   = targets.detach().cpu()
+         correct += predicted.eq(targets).sum().item()
+         total   += inputs.size(0)
          total_loss += loss
 
       return total_loss / total, correct / total
