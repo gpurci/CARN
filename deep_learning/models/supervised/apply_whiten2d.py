44e3368c6b05d8d518bd17265161f76dc04cd1da
gpurci <gepurice@gmail.com>
2026-01-16 13:58:48 +0200

    update: adapteaza modelul pentru a rula whiten

--- deep_learning/models/supervised/apply_whiten2d.py
+++ deep_learning/models/supervised/apply_whiten2d.py
@@ -9,6 +9,7 @@ class ApplyWhiten2d(nn.Module):
    def __init__(self, model, in_chanels):
       super().__init__()
       self.model = model
+      self.head  = self.model.fc
       # TO DO: add documentation about whitening operation
       # if change whiten_kernel_size, change the pad
       whiten_kernel_size = 2 # This implies a 2×2 spatial patch is used for the whitening operation
@@ -23,16 +24,22 @@ class ApplyWhiten2d(nn.Module):
       # (padding_left,padding_right, padding_top,padding_bottom)
       self.pad = (0, 1, 0, 1)
 
+      for mod in self.model.layers:
+         if isinstance(mod, nn.BatchNorm2d):
+            mod.float()
+         else:
+            mod.half()
+      self.head.half()
+
    def reset_parameters(self):
       for m in self.modules():
-         if type(m) in (nn.Conv2d, nn.BatchNorm2d):
+         if (type(m) in (nn.Conv2d, nn.BatchNorm2d)):
             m.reset_parameters()
-      w = self.model.fc.weight.data
+      w = self.head.weight.data
       w *= 1. / w.std()
-      #w = self.reduce_ch.weight.data
-      #w *= 1. / w.std()
 
-   def __transform(self, inputs, mean, std):
+   @staticmethod
+   def transform(inputs, mean, std):
       inputs = torch.from_numpy(inputs)
       inputs = (inputs - mean) / std
       inputs = torch.permute(inputs, dims=(0, 3, 1, 2))
@@ -51,8 +58,7 @@ class ApplyWhiten2d(nn.Module):
       eigenvalues, eigenvectors = torch.linalg.eigh(est_patch_covariance, UPLO="U")
       return eigenvalues.flip(0).view(-1, 1, 1, 1), eigenvectors.T.reshape(c*h*w,c,h,w).flip(0)
 
-   def init_whiten(self, inputs, mean, std, eps=5e-4):
-      inputs = self.__transform(inputs, mean, std)
+   def init_whiten(self, inputs, eps=5e-4):
       patches = ApplyWhiten2d.get_patches(inputs, self.whiten.weight.shape[2:])
       eigenvalues, eigenvectors = ApplyWhiten2d.get_whitening_parameters(patches)
       eigenvectors_scaled = eigenvectors / torch.sqrt(eigenvalues + eps)
@@ -60,8 +66,11 @@ class ApplyWhiten2d(nn.Module):
 
    def forward(self, x, whiten_bias_grad=True):
       b = self.whiten.bias
+      b_used = b if whiten_bias_grad else b.detach()
+      b_used = b_used.to(dtype=x.dtype)
       x = nn.functional.pad(x, self.pad, "constant", 0)
-      x = nn.functional.conv2d(x, self.whiten.weight, b if whiten_bias_grad else b.detach())
+      x = nn.functional.conv2d(x, self.whiten.weight.to(dtype=x.dtype), b_used)
       #x = self.reduce_ch(x)
-      x = self.model(x)
-      return x / x.size(-1)
+      x = self.model.layers(x)
+      x = x.view(len(x), -1)
+      return self.head(x) / x.size(-1)
