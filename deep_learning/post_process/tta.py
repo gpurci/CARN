c3281c2d9b1b4cf4b02c87a221be7ac4f52234bc
gpurci <gepurice@gmail.com>
2026-01-17 16:21:56 +0200

    add: post_proces/tta

---
+++ deep_learning/post_process/tta.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+
+import torch
+from torchvision.transforms import v2
+import torch.nn.functional as F
+import torchvision.transforms.v2.functional as vF
+
+class TTA():
+   def __init__(self, model, select_mixt):
+      self.model = model
+      self.__select_mixt(select_mixt)
+
+   @staticmethod
+   def help():
+      info_help = """TTA methods: basic, mixt,
+   mirror, translate, mirroring_and_translate, rotate, 
+   gray, adjust_brightness, adjust_contrast, adjust_saturation, adjust_hue, 
+   """
+      print(info_help)
+
+   def __select_mixt(self, select_mixt):
+      self.fn_mixt = []
+
+      if ("basic" in select_mixt):
+         self.fn_mixt.append(self.basic)
+      if ("mirror" in select_mixt):
+         self.fn_mixt.append(self.mirror)
+      if ("translate" in select_mixt):
+         self.fn_mixt.append(self.translate)
+      if ("mirroring_and_translate" in select_mixt):
+         self.fn_mixt.append(self.mirror_translate)
+      if ("gray" in select_mixt):
+         self.fn_mixt.append(self.gray)
+      if ("rotate" in select_mixt):
+         self.fn_mixt.append(self.rotate)
+      if ("adjust_brightness" in select_mixt):
+         self.fn_mixt.append(self.adjust_brightness)
+      if ("adjust_contrast" in select_mixt):
+         self.fn_mixt.append(self.adjust_contrast)
+      if ("adjust_saturation" in select_mixt):
+         self.fn_mixt.append(self.adjust_saturation)
+      if ("adjust_hue" in select_mixt):
+         self.fn_mixt.append(self.adjust_hue)
+      if ("mixt" in select_mixt):
+         self.fn_mixt.append(self.mixt)
+
+   def select(self, tta_type: str):
+      if   (tta_type == "mirror"):
+         self.__tta_fn = self.mirror
+      elif (tta_type == "translate"):
+         self.__tta_fn = self.translate
+      elif (tta_type == "mirroring_and_translate"):
+         self.__tta_fn = self.mirror_translate
+      elif (tta_type == "gray"):
+         self.__tta_fn = self.gray
+      elif (tta_type == "rotate"):
+         self.__tta_fn = self.rotate
+      elif (tta_type == "adjust_brightness"):
+         self.__tta_fn = self.adjust_brightness
+      elif (tta_type == "adjust_contrast"):
+         self.__tta_fn = self.adjust_contrast
+      elif (tta_type == "adjust_saturation"):
+         self.__tta_fn = self.adjust_saturation
+      elif (tta_type == "adjust_hue"):
+         self.__tta_fn = self.adjust_hue
+      elif (tta_type == "mixt"):
+         self.__tta_fn = self.mixt
+      else:
+         self.__tta_fn = self.basic
+
+   def basic(self, inputs):
+      return self.model(inputs)
+
+   def mirror(self, inputs):
+      predicted  = self.model(inputs)
+      predicted += self.model(vF.hflip(inputs))
+      predicted += self.model(vF.vflip(inputs))
+      return predicted
+
+   def translate(self, inputs):
+      pad = 2
+      padded_inputs = F.pad(inputs, (pad,)*4, "reflect")
+      inputs_list = [
+         padded_inputs[:, :, 0:32, 0:32],
+         padded_inputs[:, :, 4:36, 4:36],
+      ]
+      logits_list = [self.model(inputs_translate) for inputs_translate in inputs_list]
+      logits = torch.stack(logits_list).mean(0)
+      return logits
+
+   def mirror_translate(self, inputs):
+      logits = self.mirror(inputs)
+      pad = 2
+      padded_inputs = F.pad(inputs, (pad,)*4, "reflect")
+      inputs_pad_list = [
+         padded_inputs[:, :, 0:32, 0:32],
+         padded_inputs[:, :, 4:36, 4:36],
+      ]
+      logits_translate_list = [self.mirror(inputs_translate) for inputs_translate in inputs_pad_list]
+      logits_translate = torch.stack(logits_translate_list).mean(0)
+      return logits + logits_translate
+
+   def gray(self, inputs):
+      predicted  = self.model(inputs)
+      predicted += self.model(vF.to_grayscale(inputs, num_output_channels=3))
+      return predicted
+
+   def rotate(self, inputs):
+      predicted  = self.model(inputs)
+      predicted += self.model(vF.rotate(inputs, angle=0.2))
+      predicted += self.model(vF.rotate(inputs, angle=-0.2))
+      return predicted
+
+   def adjust_brightness(self, inputs):
+      predicted  = self.model(inputs)
+      predicted += self.model(vF.adjust_brightness(inputs, adjust_brightness=0.7))
+      predicted += self.model(vF.adjust_brightness(inputs, adjust_brightness=1.3))
+      return predicted
+
+   def adjust_contrast(self, inputs):
+      predicted  = self.model(inputs)
+      predicted += self.model(vF.adjust_contrast(inputs, contrast_factor=0.7))
+      predicted += self.model(vF.adjust_contrast(inputs, contrast_factor=1.3))
+      return predicted
+
+   def adjust_saturation(self, inputs):
+      predicted  = self.model(inputs)
+      predicted += self.model(vF.adjust_saturation(inputs, saturation_factor=0.7))
+      predicted += self.model(vF.adjust_saturation(inputs, saturation_factor=1.3))
+      return predicted
+
+   def adjust_hue(self, inputs):
+      predicted  = self.model(inputs)
+      predicted += self.model(vF.adjust_hue(inputs, hue_factor=0.15))
+      predicted += self.model(vF.adjust_hue(inputs, hue_factor=-0.15))
+      return predicted
+
+   def mixt(self, inputs):
+      logits_list = []
+      for fn in self.fn_mixt:
+         logits_list.append(fn(inputs))
+      return torch.stack(logits_list).mean(0)
+
+   def __call__(self, x):
+      return self.__tta_fn(x)
