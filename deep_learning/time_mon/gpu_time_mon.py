f1fc1c436913d2974d859a3cbc32b717e59eb0c8
gpurci <gepurice@gmail.com>
2026-01-20 19:45:53 +0200

    update: adauga reset

--- deep_learning/time_mon/gpu_time_mon.py
+++ deep_learning/time_mon/gpu_time_mon.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
 
-import pandas as pd
-from pathlib import Path
-import warnings
+import torch
 
 class GPUTimeMon(object):
-   def __init__():
+   def __init__(self):
       # For accurately timing GPU code
       self.starter = torch.cuda.Event(enable_timing=True)
-      self.ender = torch.cuda.Event(enable_timing=True)
+      self.ender   = torch.cuda.Event(enable_timing=True)
       self.time_seconds = 0.0
 
-   def start_timer(self):
+   def start(self):
       self.starter.record()
 
-   def stop_timer(self):
+   def stop(self):
       self.ender.record()
-      self.torch.cuda.synchronize()
+      torch.cuda.synchronize()
       self.time_seconds += 1e-3 * self.starter.elapsed_time(self.ender)
+
+   def time(self):
+      return self.time_seconds
+
+   def reset(self):
+      self.time_seconds = 0.0
