c7fb08b0eda2c23b92b45b7878f07938a0649a30
gpurci <gepurice@gmail.com>
2026-01-25 12:16:36 +0200

    rename: save_best_acc_val -> save_model_by_best_key

--- deep_learning/callback/save_model_by_best_key.py
+++ deep_learning/callback/save_model_by_best_key.py
@@ -5,17 +5,18 @@ from pathlib import Path
 import numpy as np
 from callback.callback_base import *
 
-class SaveBestAccVal(CallbacksBase):
-   def __init__(self, model, filename):
+class SaveModelByBestKey(CallbacksBase):
+   def __init__(self, model, filename, key="val_acc"):
       """"""
-      self.model    = model
+      self.setModel(model)
       self.filename = filename
-      self.best_acc = 0
+      self.key      = key
+      self.val_best_key = 0
       path = Path(filename).parent
       Path(path).mkdir(mode=0o777, parents=True, exist_ok=True)
       Path(filename).touch(mode=0o666, exist_ok=True)
 
    def on_epoch_end(self, epoch, logs=None):
-      if (("val_acc" in logs) and (logs["val_acc"] > self.best_acc)):
-         self.best_acc = logs["val_acc"]
+      if ((self.key in logs) and (logs[self.key] > self.val_best_key)):
+         self.val_best_key = logs[self.key]
          torch.save({"model_state_dict": self.model.state_dict()}, self.filename)
