72290401fde42ecc3ab87adb03b783ce68e66b86
gpurci <gepurice@gmail.com>
2026-02-10 11:50:26 +0200

    bug fix: corecteaza calculul functiei de cost MAE+MSE

--- deep_learning/loss/mae_mse_loss.py
+++ deep_learning/loss/mae_mse_loss.py
@@ -4,11 +4,22 @@ import torch
 from torch import nn
 
 class MAEMSELoss(nn.Module):
-   def __init__(self, alpha=0.5):
+   def __init__(self, alpha=0.5, reduction="mean"):
       super().__init__()
       self.alpha = nn.Parameter(torch.tensor(alpha))
+      self.reduction = reduction
 
    def forward(self, pred, target):
-      l1 = torch.mean(torch.abs(pred - target))
-      l2 = torch.mean((pred - target) ** 2)
+      l1 = torch.abs(pred - target)
+      l2 = (pred - target) ** 2
+
+      if   (self.reduction == "mean"):
+         l1 = torch.mean(l1)
+         l2 = torch.mean(l2)
+      elif (self.reduction == "sum"):
+         l1 = torch.sum(l1)
+         l2 = torch.sum(l2)
+      elif (self.reduction == "none"):
+         pass
+
       return self.alpha * l1 + (1 - self.alpha) * l2
