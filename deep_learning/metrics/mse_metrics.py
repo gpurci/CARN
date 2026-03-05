5aed0e20b62054a03b3b1fcfba30afebf15c6016
gpurci <gepurice@gmail.com>
2026-03-05 11:47:55 +0200

    update: calculeaza argmax pentru y_prediction in cazul in care se cere

--- deep_learning/metrics/mse_metrics.py
+++ deep_learning/metrics/mse_metrics.py
@@ -11,5 +11,7 @@ class MSEMetrics(MetricsBase):
    def __call__(self, y, y_pred):
       # y     : true labels (batch_size,)
       # y_pred: predicted labels (batch_size, )
+      if (len(y.shape) != len(y_pred.shape)):
+         y_pred = torch.argmax(y_pred, dim=1).float()
       mse = nn.functional.mse_loss(y, y_pred, size_average=None, reduce=None, reduction="mean", weight=None)
-      return mse
+      return mse.item()
